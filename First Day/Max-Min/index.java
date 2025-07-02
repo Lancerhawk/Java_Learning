85a499573c856184bc8a8f63eb2892220d87c4fb
Adrian <70001933arin@gmail.com>
2025-07-02 11:54:22 +0530

    Modified with Use Input

--- First Day/Max-Min/index.java
+++ First Day/Max-Min/index.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class index
 {
     public static void main(String[] args)
     {
-        int[] arr = {1, 3, 5, 2, 8, 4};
+        // int[] arr = {1, 3, 5, 2, 8, 4};
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the Size of the Array: ");
+        
+        int s = input.nextInt();
+        int arr[] = new int[s];
+
+        for (int x = 0; x < s; x++)
+        {
+            
+            System.out.println("Give the "+x+ " index for the array: ");
+            int value = input.nextInt();
+            arr[x] = value;
+
+        }
 
         int max = arr[0];
         int min = arr[0];
--- First Day/Sum_Array/index.java
+++ First Day/Sum_Array/index.java
@@ -1,13 +1,31 @@
-public class index{
-    public static void main(String[] args){
-        int[] arr = {1, 4, 2, 3, 5};
+import java.util.Scanner;
 
+public class index{
+    public static void main(String[] args)
+    {
+        // int[] arr = {1, 4, 2, 3, 5};
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the Size of the Array: ");
+        
+        int s = input.nextInt();
+        int arr[] = new int[s];
+
+        for (int x = 0; x < s; x++)
+        {
+            
+            System.out.println("Give the "+x+ " index for the array: ");
+            int value = input.nextInt();
+            arr[x] = value;
+
+        }
+        
         int sum = 0;
 
-        for(int i = 0; i < arr.length; i++){
+        for(int i = 0; i < arr.length; i++)
+        {
             sum += arr[i];
         }
-
 
         System.out.println("The sum of all the numbers in the array is: "+sum);
     }
