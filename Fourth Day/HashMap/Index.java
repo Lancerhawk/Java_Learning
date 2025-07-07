466b31bb9e126a465c4e62cb1c2bb4c1568f33b6
Adrian <70001933arin@gmail.com>
2025-07-07 11:54:24 +0530

    fourth day

---
+++ Fourth Day/HashMap/Index.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Index
+{
+    public static void main(String[] args)
+    {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        String letter = "Programminggggggg";
+
+        for(char c : letter.toCharArray())
+        {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+
+        System.out.println(map);
+
+    }
+}
---
+++ Fourth Day/Set_TreeSet/Index.java
@@ -0,0 +1,17 @@
+import java.util.TreeSet;
+
+public class Index{
+    public static void main(String[] args){
+        TreeSet<Integer> set = new TreeSet<>();
+
+        //Auto Sorts the data or elements within
+
+        set.add(2);
+        set.add(4);
+        set.add(22);
+        set.add(1);
+        set.add(12);
+
+        System.out.println(set);
+    }
+}
